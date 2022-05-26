780c887a44aca2929ef9cb863b99c7c8e142c027
PedroMarMol <martosmoleropedro@gmail.com>
2022-05-26 12:40:26 +0200

    added home tab js

---
+++ src/home.js
@@ -0,0 +1,29 @@
+function createHome() {
+    const home = document.createElement('div');
+    home.classList.add('home');
+    
+    const restaurantImage = document.createElement('img');
+    restaurantImage.src = '../images/restaurant.jpg';
+    restaurantImage.alt = 'restaurant place';
+
+    home.appendChild(createParagraph('A wide variety of really well made burgers'))
+    home.appendChild(createParagraph('Working with eco friendly products'))
+    home.appendChild(restaurantImage)
+    home.appendChild(createParagraph("Come and visit us or order online!"))
+
+    return home;
+}
+
+function createParagraph(text) {
+    const paragraph = document.createElement('p');
+    paragraph.textContent = text;
+    return paragraph;
+}
+
+function loadHome() {
+    const main = document.getElementById('main');
+    main.textContent = '';
+    main.appendChild(createHome());
+}
+
+export default loadHome;
