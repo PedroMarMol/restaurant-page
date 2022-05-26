8c2941bd7e96a97e97b64363a6e9ac7a4ca4875c
PedroMarMol <martosmoleropedro@gmail.com>
2022-05-26 12:16:56 +0200

    added contact section js

---
+++ src/contact.js
@@ -0,0 +1,7 @@
+function createContact() {
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+
+    const phoneNumber = document.createElement('p')
+    phoneNumber.textContent = '📱 609 445 580'
+}
